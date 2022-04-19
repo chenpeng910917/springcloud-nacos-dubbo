fbcb143abc55a10698c12ed12fc94a1fde947d7a
chenpeng <917chenpeng>
2022-04-19 22:59:46 +0800

    springcloud-nacos-dubbo

---
+++ dubbo-consumer-interface/src/test/java/com/consumer/dubboconsumerinterface/DubboConsumerInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumer.dubboconsumerinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboConsumerInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ dubbo-provider-interface/src/main/java/com/provider/dubboproviderinterface/DubboProviderInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.provider.dubboproviderinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboProviderInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboProviderInterfaceApplication.class, args);
+    }
+
+}
