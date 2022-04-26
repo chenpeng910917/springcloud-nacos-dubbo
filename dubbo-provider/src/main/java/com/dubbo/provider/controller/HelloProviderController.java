5e9c4e6393e760c2bd5f02ee8a2d2cec915d8eed
陈鹏 <917chenpeng>
2022-04-26 14:00:38 +0800

    springcloud-nacos-dubbo-config

--- dubbo-provider/src/main/java/com/dubbo/provider/controller/HelloProviderController.java
+++ dubbo-provider/src/main/java/com/dubbo/provider/controller/HelloProviderController.java
@@ -3,8 +3,6 @@ package com.dubbo.provider.controller;
 import com.consumer.rpc.HelloConsumerRpc;
 import com.consumer.rpc.model.param.HelloConsumerParam;
 import com.consumer.rpc.model.vo.HelloConsumerVO;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author chenpeng
  */
-@Api(tags = "HelloProviderController")
 @Slf4j
 @RestController
 public class HelloProviderController {
@@ -27,7 +24,6 @@ public class HelloProviderController {
      * @param param 入参
      * @return 反参
      */
-    @ApiOperation("你好")
     @GetMapping(value = "/hello")
     public HelloConsumerVO hello(HelloConsumerParam param) {
         log.info("HelloProviderController param={}", param);
